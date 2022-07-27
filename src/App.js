1e800f3d8f1ca51e9a50e4bf845b4c8bf26d69d5
harry536 <singhharendra255@gmail.com>
2022-07-27 18:05:35 +0530

    null

--- src/App.js
+++ src/App.js
@@ -14,10 +14,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home/>} />
           <Route path='/services' element={<Services/>} />
           <Route path='/products' element={<Products/>} />
           <Route path='/sign-up' element={<SignUp/>} />
+          <Route path='/*' exact element={<Home/>} />
         </Routes>
       </Router>
     </>
